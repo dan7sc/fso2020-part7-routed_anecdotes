7c31f8cf864d9fffb0f059748db6988431969939
dan7sc <su7cheng@gmail.com>
2020-05-17 14:14:48 -0300

    Add solution to exercise 7.5: anecdotes and hooks step2

--- src/App.js
+++ src/App.js
@@ -83,10 +83,16 @@ const CreateNew = (props) => {
     })
   }
 
+  const handleReset = () => {
+    content.onReset()
+    author.onReset()
+    info.onReset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} onReset={handleReset}>
         <div>
           content
           <input name='content' {...content} />
@@ -99,7 +105,8 @@ const CreateNew = (props) => {
           url for more info
           <input name='info' {...info} />
         </div>
-        <button>create</button>
+        <button type='submit'>create</button>
+        <button type='reset'>reset</button>
       </form>
     </div>
   )
